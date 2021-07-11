93b050de0a8326d55ddc85228ee88d59f592f618
Aditya Arya <Aditya_arya@aol.com>
2021-07-11 13:59:07 +0530

    Export1

---
+++ File1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <ctype.h>
+#include <<string.h>
+#include <stdlib.h>
+char ans=0;
+int ok;
+int b,valid=0;
+
+//Function Declaration
+void WelcomeScreen(void);
+void Title(void);
+void MainMenu(void);
+void LoginScreen(void);
+void Add_rec(void);
+void func_list(void);
+void Search_rec(void);
+void Edit_rec(void);
+void Dlt_rec(void);
+void ex_it(void);
+void gotoxy(short x, short y)
+{
+    COORD pos
+}
